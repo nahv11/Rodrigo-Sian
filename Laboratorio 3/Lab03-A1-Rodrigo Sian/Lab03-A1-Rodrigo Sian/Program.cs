102e0ce3b1ba19429918d826ed8f126b416214ce
nahv11 <resian@correo.url.edu.gt>
2026-02-04 08:08:04 -0600

    Laboratorio 3
    Subiendo el lab 3

---
+++ Laboratorio 3/Lab03-A1-Rodrigo Sian/Lab03-A1-Rodrigo Sian/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Laboratorio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombreal, nombrecur;
+
+            Console.WriteLine("Ingresa el nombre del curso: ");
+            nombrecur = Console.ReadLine();
+            Console.WriteLine("Ingresa el nombre del estudiante: ");
+            nombreal = Console.ReadLine();
+            Console.WriteLine("El nombre del estudiante es: "+nombreal+ "\nEl nombre del curso es: " + nombrecur);
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
+
